9ce3d7365083f2667c927ab46c7331734c4f7fa8
khaly <khalyruot12345@gmail.com>
2024-08-22 18:21:00 -0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const myLibrary = [];
 
 function Book(author, title, page, status) {
     this.author = author;
     this.title = title;
     this.page = page;
     this.status = status;
+    this.addBookToLibrary = function() {
+        console.log(this.status);
+      };
 }
 
-function addBookToLibrary(){
-
-}
+const book1 = new Book("Alice", "Word of Wisdom", 10, true);
+book1.addBookToLibrary();
