75ee38d5e3b82f848f040847e9d441920abed8de
khaly <khalyruot12345@gmail.com>
2024-08-24 13:35:52 -0700

    work on html

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const myLibrary = [];
 
 function Book(author, title, page, status) {
     this.author = author;
     this.title = title;
     this.page = page;
     this.status = status;
-    this.addBookToLibrary = function() {
-        console.log(this.status);
-      };
 }
 
-const book1 = new Book("Alice", "Word of Wisdom", 10, true);
-book1.addBookToLibrary();
+function inputBook(){
+
+}
+
+function addBookToLibrary(){
+
+  console.log("hello");
+ 
+}
+
+
+
+
