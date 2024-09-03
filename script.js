2c43a7628356bde14f986c45406785a5aec995e7
khaly <khalyruot12345@gmail.com>
2024-09-03 09:48:32 +0700

    add table

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 let myLibrary = [];
 
-
-
-function Book(author, title, page, status) {
-    this.author = author;
-    this.title = title;
-    this.page = page;
-    this.status = status;
-}
-
-function addBookToLibrary(){
   const author_input = document.getElementById('input1');
   const title_input = document.getElementById('input2');
   const page_input = document.getElementById('input3');
@@ -19,24 +9,43 @@ function addBookToLibrary(){
   const display_output_title = document.querySelector('#display_title');
   const display_output_page = document.querySelector('#display_page');
   const display_output_status = document.querySelector('#display_status');
+
+
+
+function Book(author, title, page, status) {
+    this.author = author;
+    this.title = title;
+    this.page = page;
+    this.status = status;
+}
+
+function addBookToLibrary(event){
+
+  event.preventDefault();
   
-  const book = [{author: author_input.value, title: title_input.value, page: page_input.value, status: status_input.value}]
+
   
   
 
+  displayBook();
+  
+}
+
+function displayBook(){
+
+  const book = [{author: author_input.value, title: title_input.value, page: page_input.value, status: status_input.value}]
   book.forEach(value=>{
     myLibrary =  "Author: " + value.author + ", Title: " + value.title + ", Page:" + value.page + ", Status: " + value.status;
     console.log(myLibrary);
     console.log(title_input.value);
-    display_output_author.innerHTML = "The Author is: " + author_input.value;
-    display_output_title.innerHTML = "The Title is: " + title_input.value;
-    display_output_page.innerHTML = "The page number is: " + page_input.value;
-    display_output_status.innerHTML = "The Status is: " + status_input.value;
+    display_output_author.innerHTML = author_input.value;
+    display_output_title.innerHTML = title_input.value;
+    display_output_page.innerHTML = page_input.value;
+    display_output_status.innerHTML = status_input.value;
     
   });
-  
-}
 
+}
 
 
 
